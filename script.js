18a71b76ef225286a16ad0e21b24c77221a53b9b
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-12 19:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.let
-let load=document.getElementsByTagName("body");
-load.addEventListener("load",()=>{
-	console.log("DOM load success");
-})
+function abc(){
+	let para=document.getElementById("demo").innerText="DOM load success"
+}
 
 
